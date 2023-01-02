6ecdc7d837fee289549b3f54faf87dbe3d8f51e8
Witold Weiner <intreks489@gmail.com>
2023-01-03 00:51:17 +0100

    naprawienie gry - na starych zasadach - do zmiany system upgradow

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 console.log("hello world")
 var Rekord = 0;
-var points_per_click = 1;
+var points_per_click = 2;
+var points_per_second = 100;
+var PPC_update_counter = 1;
 
-
-document.querySelector(".infos").innerText = "Your record is "+Rekord+".\n"+"Points per click: "+points_per_click+". \n";
-
+  document.querySelector(".infos").innerText = "Your record is \n"
+        +Rekord.toFixed(2)+"\n"+"Points per click: \n"
+            +points_per_click.toFixed(2)+" \n"+"Points per second: \n"
+                +points_per_second.toFixed(2)+" \n";
+                
     const tmp =  document.querySelector(".clicker")
     console.log(tmp)
     tmp.addEventListener("click",click)
@@ -20,41 +24,63 @@ document.querySelector(".infos").innerText = "Your record is "+Rekord+".\n"+"Poi
    const button5= document.querySelector("#button5");
    button5.addEventListener("click", () => Upgrade(5) );
 
+   const button6= document.querySelector("#button6");
+   button6.addEventListener("click", () => Upgrade(6) );
+
+function updateInfos()
+{
+    document.querySelector(".infos").innerText = "Your record is \n"
+        +Rekord.toFixed(2)+"\n"+"Points per click: \n"
+            +points_per_click.toFixed(2)+" \n"+"Points per second: \n"
+                +points_per_second.toFixed(2)+" \n";
+}
 function click ()
 {
     Rekord += points_per_click;
-    document.querySelector(".infos").innerText = "Your record is "+Rekord+".\n"+"Points per click: "+points_per_click+". \n";
-    console.log(Rekord)
+    updateInfos();
 }
 function Upgrade (upgradeID )
 {
     if (upgradeID == 1 && Rekord >= 10  )
     {
-        points_per_click = 2
+        points_per_second += 0.01
         Rekord -=10
     }
-    if (upgradeID == 2 && Rekord >= 100  )
+    if (upgradeID == 2 && Rekord >= 80  )
     {
-        points_per_click = 5
-        Rekord -=100
+        points_per_second += 0.1
+        Rekord -=80
     }
     if (upgradeID == 3 && Rekord >= 250  )
     {
-        points_per_click = 10
+        points_per_second += 1
         Rekord -=250
     }
-    if (upgradeID == 4 && Rekord >= 500 )
+    if (upgradeID == 4 && Rekord >= 750 )
     {
-        points_per_click = 100
-        Rekord -=500
+        points_per_second += 10
+        Rekord -=750
     }
-    if (upgradeID == 5 && Rekord >= 10000  )
+    if (upgradeID == 5 && Rekord >= 3000  )
     {
-        points_per_click = 250
-        Rekord -=10000
+        points_per_second += 100
+        Rekord -=3000
     }
-    console.log(points_per_click)
-    console.log(Rekord)
-    document.querySelector(".infos").innerText = "Your record is "+Rekord+".\n"+"Points per click: "+points_per_click+". \n";
 
+    if (upgradeID == 6 && Rekord >= 100*PPC_update_counter)
+        {
+            points_per_click += 0.1*points_per_click;
+            Rekord -= 100*PPC_update_counter;
+            PPC_update_counter++;
+
+        }
+
+    updateInfos()
 }
+
+setInterval( () =>
+{
+    Rekord = Rekord + points_per_second
+    updateInfos()
+
+} ,1000)
